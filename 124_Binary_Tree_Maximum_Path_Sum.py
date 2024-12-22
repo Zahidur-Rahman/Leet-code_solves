42d0f2355bae461be03711396a3274896750ac5e
Zahidur-Rahman <pzahidurtan780550@gmail.com>
2024-12-22 11:41:29 +0600

    Some Problem Solved

--- 124_Binary_Tree_Maximum_Path_Sum.py
+++ 124_Binary_Tree_Maximum_Path_Sum.py
@@ -12,6 +12,15 @@ class Solution:
                 return 0
             
             leftMax=dfs(root.left)
-            rightMax=dfs(root.right)
             
-             
+            rightMax=dfs(root.right)
+            leftMax=max(leftMax,0)
+            rightMax=max(rightMax,0)
+            
+            
+           
+            res[0]=max(res[0], root.val+leftMax+rightMax)
+            
+            return root.val+max(leftMax,rightMax)
+        dfs(root)
+        return res[0]
